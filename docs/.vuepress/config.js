a4c39b47c185173c271b9da80dde6682aeb053de
只会番茄炒蛋 <673395239@qq.com>
2020-01-20 15:13:34 +0800

    修改配置信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,90 +1,105 @@
 module.exports = {
-    title: '只会番茄炒蛋', // 左上角标题
-    description: '高宇的个人网站，记录生活的瞬间，分享学习的心得，感悟生活，留住感动，静静寻觅生活的美好', // 网站描述, 用来SEO
-    dest: 'dist', // dest默认值为.vuepress/dist，配置它可以显示的帮助我们设置打包文件的输出目录
-    port: 8080,
-    host: '127.0.0.1',
-    head: [
-      ['link', { rel: 'icon', href: '/favicon.ico' }]
+  title: '只会番茄炒蛋', // 左上角标题
+  description: '高宇的个人网站，记录生活的瞬间，分享学习的心得，感悟生活，留住感动，静静寻觅生活的美好', // 网站描述, 用来SEO
+  dest: 'dist', // dest默认值为.vuepress/dist，配置它可以显示的帮助我们设置打包文件的输出目录
+  port: 8080,
+  host: '127.0.0.1',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  // 其它配置
+  base: '/vuePressBlog/',
+  themeConfig: {
+    repo: 'https://github.com/zhihuifanqiechaodan', // nav最后一个GitHub链接
+    repoLabel: 'Github',
+    nav: [
+      { text: '首页', link: '/home/' },
+      // {   text: 'HTML', 
+      //     items: [
+      //     { text: 'HTML', link: '/HTML/' },
+      //     { text: 'CSS', link: '/CSS/' },
+      //     { text: 'JavaScript', link: '/JavaScript/' }
+      //   ] 
+      // },
+      { text: '前端体系', link: '/note/' },
+      { text: '进阶文章', link: '/blog/' },
+      { text: '每日壹题', link: '/question/' },
+      { text: '个人简历', link: '/resume/' },
     ],
-    // 其它配置
-    base: '/vuePressBlog/',
-    themeConfig: {
-      repo: 'https://github.com/zhihuifanqiechaodan', // nav最后一个GitHub链接
-      repoLabel: 'Github',
-      nav: [
-        {   text: '首页', link: '/home/' },
-        // {   text: 'HTML', 
-        //     items: [
-        //     { text: 'HTML', link: '/HTML/' },
-        //     { text: 'CSS', link: '/CSS/' },
-        //     { text: 'JavaScript', link: '/JavaScript/' }
-        //   ] 
-        // },
-        {   text: '前端体系', link: '/note/' },
-        {   text: '进阶文章', link: '/blog/' },
-        {   text: '每日壹题', link: '/question/' },
-        {   text: '个人简历', link: '/resume/' },
+    sidebar: {
+      '/note/': [
+        {
+          title: 'Vue',
+          children: [
+            '/note/vue/基础学习笔记',
+            '/note/vue/进阶学习笔记',
+            '/note/vue/源码解析之手写Vue源码',
+            '/note/vue/进度条和数据加载Loading',
+            '/note/vue/跨域以及打包部署到nginx跨域',
+            '/note/vue/使用阿里iconfont图标',
+            '/note/vue/使用富文本',
+            '/note/vue/导出Excel',
+          ]
+        },
+        {
+          title: 'React',
+          children: [
+            '/note/react/react基础学习笔记',
+            '/note/react/如何在React中使用Redux'
+          ]
+        },
+        {
+          title: 'JavaScript',
+          children: [
+            '/note/javascript/基础学习笔记',
+            '/note/javascript/进阶学习笔记',
+            '/note/javascript/面向对象编程',
+            '/note/javascript/函数提升与变量提升',
+          ]
+        },
+        {
+          title: 'TypeScript',
+          children: [
+            '/note/typescript/入门TypeScript'
+          ]
+        },
+        {
+          title: '服务器构建',
+          children: [
+            '/note/server/阿里云服务器ECS配置及LAMP环境搭建及配置',
+            '/note/server/Jenkins自动打包部署vue项目'
+          ]
+        },
       ],
-      sidebar: {
-        '/note/': [
-          {
-            title: '1.HTML',
-              children: [
-                '/note/html/html基础学习笔记',
-                '/note/html/html5基础学习笔记'
-              ]
-          },
-          {
-            title: '2.CSS',
-              children: [
-                '/note/css/test'
-              ]
-          },
-          {
-            title: '3.JavaScript',
-              children: [
-                '/note/javascript/javascript基础学习笔记',
-                '/note/javascript/javascript进阶学习笔记'
-              ]
-          },
-          {
-            title: '4.Vue',
-              children: [
-                '/note/vue/vue2.0基础学习笔记',
-                '/note/vue/vue2.0进阶学习笔记'
-              ]
-          }
-        ],
-        '/question/': [
-          {
-            title: '1.JavaScript',
-              children: [
-                '/question/javascript/javascript进阶问题汇总' // 以docs为根目录来查找文件 
-              ]
-          }
-        ],
-        '/': [
-          ''
-        ]
-      },
-      search: true,                // 通过配置此属性为false，来禁用内置搜索
-      searchMaxSuggestions: 10,    // 通过配置此属性为一个数字，对内置的搜索进行最多结果数量的限制
-      lastUpdated: true,           // 设置true，开启最后更新时间
-      // algolia: { // 配置全文搜索需要Algolia官网上添加自己的这个网址
-      //   apiKey: '<API_KEY>',
-      //   indexName: '<INDEX_NAME>'
-      // }
-    },
-    markdown: {
-      // 显示代码块行号
-      lineNumbers: true
-    },
-    configureWebpack: {
-      resolve: {
-        alias: {
-          '@images': '../images'
+      '/question/': [
+        {
+          title: '1.JavaScript',
+          children: [
+            '/question/javascript/javascript进阶问题汇总' // 以docs为根目录来查找文件 
+          ]
         }
+      ],
+      '/': [
+        ''
+      ]
+    },
+    search: true,                // 通过配置此属性为false，来禁用内置搜索
+    searchMaxSuggestions: 10,    // 通过配置此属性为一个数字，对内置的搜索进行最多结果数量的限制
+    lastUpdated: true,           // 设置true，开启最后更新时间
+    // algolia: { // 配置全文搜索需要Algolia官网上添加自己的这个网址
+    //   apiKey: '<API_KEY>',
+    //   indexName: '<INDEX_NAME>'
+    // }
+  },
+  markdown: {
+    // 显示代码块行号
+    lineNumbers: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@images': '../images'
       }
     }
   }
+}
